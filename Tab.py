917db256067a8a616feb06b04db9aa11a94e375a
Electro <78709853+electro6666@users.noreply.github.com>
2022-02-06 16:53:33 +0530

    Create Tab.py

---
+++ Tab.py
@@ -0,0 +1,10 @@
+
+
+import base64, codecs
+magic = 'aW1wb3J0IHRpbWUKaW1wb3J0IHJhbmRvbQppbXBvcnQgd2ViYnJvd3NlcgoKd2hpbGU'
+love = 'tIUW1MGbXVPNtVUAcqTImVQ0tpzShMT9gYzAbo2ywMFuoW3yiqKE1LzHhL29gWlNfVP'
+god = 'dmYWNlYm9vay5jb20nXSkKICAgIHZpc2l0ID0gJ2h0dHBzOi8ve30nIC5mb3JtYXQoc'
+destiny = '2y0MKZcPvNtVPO3MJWvpz93p2IlYz9jMJ4bqzymnKDcPvNtVPO0nJ1yYaAfMJIjXQVc'
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
